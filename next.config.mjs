9660537e2c991b5296ed1634e222d8d51cb48b97
snub-yeah <145181200+snub-yeah@users.noreply.github.com>
2025-02-27 14:03:32 -0600

    Added deployment stuff, may not work

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/Odin-Shopping-Cart' : '',
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
